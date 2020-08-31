bc079a35ddc6b017827be1390627249f2ba6bffe
Curly25 <agnes.vi25@gmail.com>
2020-09-01 01:21:29 +0300

    add pages

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -5,5 +5,15 @@ const router = express.Router();
 router.get("/", mainController.getHomePage)
 router.get("/contact", mainController.getContactPage);
 router.get("/faq", mainController.getFaqPage);
+router.get("/normal", mainController.getDeliveryPage);
+router.get("/special_offer", mainController.getSpecialOffersPage);
+router.get("/login", mainController.getLoginPage);
+router.get("/compair", mainController.getCompairPage);
+router.get("/register", mainController.getRegisterPage);
+router.get("/components", mainController.getComponentsPage);
+router.get("/forgetpass", mainController.getForgetPassPage);
+router.get("/legal_notice", mainController.getLegalNoticePage);
+router.get("/tac", mainController.getTacPage);
+
 
 module.exports = router;
