234eb465f2926abba0e71b195a0a4dc9329b79f3
Curly25 <agnes.vi25@gmail.com>
2020-09-17 14:56:19 +0300

    add admin

--- helper/database.js
+++ helper/database.js
@@ -1,17 +1,9 @@
 const Sequalize = require("sequelize");
-const DBHOST = process.env.DBHOST;
-const DBUSERNAME = process.env.DBUSERNAME;
-const PASSWORD = process.env.PASSWORD;
-const DATABASENAME = process.env.DATABASENAME;
-
-console.log("HOSTNAME", DBHOST);
-console.log("DBUSERNAME", DBUSERNAME);
-console.log("DATABASENAME", DATABASENAME);
-console.log("PASSWORD", PASSWORD);
-
-const sequalize = new Sequalize(DATABASENAME, DBUSERNAME, PASSWORD, {
-  dialect: "mysql",
-  host: DBHOST,
+const { DATABASENAME, PASSWORD, USERNAME, HOSTNAME } = require("./config");
+const sequalize = new Sequalize(DATABASENAME, PASSWORD, USERNAME, {
+	dialect: "mysql",
+	//host: "10.7.101.193", // internal address 
+	host: HOSTNAME, // from internet addres
 });
 
 module.exports = sequalize;
